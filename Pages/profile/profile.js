138f9c60ef3d49ead2507cdc36a7fb6bdbecf8d1
Yjaaaaa <320425675@qq.com>
2021-06-22 13:18:52 +0800

    注册小程序和页面

--- Pages/profile/profile.js
+++ Pages/profile/profile.js
@@ -1,91 +1,66 @@
-// home/home.js
+// pages/profile/profile.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    list: []
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log('onLoad')
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    console.log('onReady')
-    wx.request({
-      url: 'http://152.136.185.210:7878/api/m5/home/data?type=sell&page=1',
-      success: (res) => {
-        console.log(res)
-        const data = res.data.data.list;
-        this.setData({
-          list: data
-        })
-      }
-    })
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    console.log('onShow')
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    console.log('onHide')
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    console.log('onUnload')
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    console.log('onLoad')
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
-  },
-    /**
-   * 监听页面的滚动
-   */
-  onPageScroll(obj) {
-    // console.log(obj)
-  },
-  onReachBottom() {
-    console.log('----')
-  },
-  onPullDownRefresh() {
-    console.log('下拉刷新')
+
   }
 })
